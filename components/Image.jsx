937f229b0246eb72aa8be02e8dfb6130e85df672
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-09-21 13:03:16 +0100

    Fix gap

--- components/Image.jsx
+++ components/Image.jsx
@@ -15,7 +15,7 @@ const ImageCard = ({ image }) => {
 			<div className='block relative xs:h-[120px] sm:h-[150px]  md:h-[200px] lg:h-[250px] rounded-xl shadow-lg '>
 				<Image placeholder='empty' blurDataURL={image?.blur_hash || 'LJJ*esOZ?w-;~XsmaJxvT1t6jEbI'} src={image?.urls?.small} layout='fill' objectFit='cover' alt='image' className={`${isDragging ? 'opacity-50 bg-[rgba(0,0,0,0.8)]' : 'opacity-100'} rounded-md aspect-square bg-blend-color bg-center`} />
 			</div>
-			<ul className='flex w-full bg-white gap-3 justify-start flex-nowrap'>
+			<ul className='flex w-full bg-white gap-1 justify-start flex-nowrap'>
 				{image?.tags?.slice(0, 2).map((tag) => (
 					<li key={tag?.title.slice(0, 7)} className='shadow-md text-[12px] rounded-md border border-gray-200 md:text-[14px] lg:text-base p-1 py-0 mt-1'>
 						{tag?.title.slice(0, 10)}
